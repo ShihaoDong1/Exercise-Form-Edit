c8aaa57596e6e8200c679a0df93c84012634009b
Nicole <nwoje13@gmail.com>
2020-06-03 16:51:01 -0400

    updated Form

--- src/Form.js
+++ src/Form.js
@@ -1,41 +1,61 @@
 import React, { Component } from "react";
 
-class UserInfo  extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            didClickEdit:false
-        }
-    }
+class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: "",
+      lastName: "",
+      isClickEdit: false,
+    };
 
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  
+  handleEdit = () => {
+    this.setState({ 
+      isClickEdit: true 
+    });
+  };
 
-    changeInfo = () => {
-        this.setState({
-            didClickEdit: true
-        })
-    }
-
-    render() {
-        if(this.state.didClickEdit) {
-            
-        }
-        else {
-            return(
-                <div >
-                    <p>
-                        <label>FirstName: </label>
-                    </p>
-                    <p>
-                        <label>LastName: </label>
-                    </p>
-                    <button onClick={this.changeInfo}>Edit</button>
-                </div>
-            );
-        }
-
-
-      
-    }
+	handleNameChange(event) {
+    this.setState({
+      firstName: event.target.value,
+      lastName: event.target.value
+    });
+  }
+	
+	render() {
+    const edit = this.state.isClickEdit === false ? 
+    (
+      <>
+      <label>FirstName: {this.state.firstName}</label>
+      <br></br>
+      <label>LastName: {this.state.lastName}</label>
+      <br></br>
+      <button onClick={this.handleEdit}>Edit</button>
+      </>
+    ) : 
+    ( 
+      <div>
+      <p>First name:</p>
+      <input
+        type="text"
+        firstName={this.state.firstName}
+        onChange={this.handleNameChange}
+      />
+      <p>Last name:</p>
+      <input
+        type="text"
+        lastName={this.state.lastName}
+        onChange={this.handleNameChange}
+      />
+      </div>
+    )
+    return (
+      <div>{edit}</div>
+    );
+  }
 }
 
-export default UserInfo
+export default Form;
