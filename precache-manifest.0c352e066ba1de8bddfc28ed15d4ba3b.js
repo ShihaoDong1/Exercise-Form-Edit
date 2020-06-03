b69ff7c5bcebc6e400a244f4e69dbb8c615ca1f3
ShihaoDong1 <shihao.dong1@gmail.com>
2020-06-03 19:36:14 -0400

    Updates

---
+++ precache-manifest.0c352e066ba1de8bddfc28ed15d4ba3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c986767e2bf0711ba4bf343e87c0ec01",
+    "url": "/Exercise-Form-Edit/index.html"
+  },
+  {
+    "revision": "d94a329a9b86508b9f41",
+    "url": "/Exercise-Form-Edit/static/css/main.e90a9ddc.chunk.css"
+  },
+  {
+    "revision": "00f6eb56da3c663c2fbd",
+    "url": "/Exercise-Form-Edit/static/js/2.4991f22d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Exercise-Form-Edit/static/js/2.4991f22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94a329a9b86508b9f41",
+    "url": "/Exercise-Form-Edit/static/js/main.1caef7a1.chunk.js"
+  },
+  {
+    "revision": "bfef4c4691d1085cb979",
+    "url": "/Exercise-Form-Edit/static/js/runtime-main.528da2dc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Exercise-Form-Edit/static/media/logo.ee7cd8ed.svg"
+  }
+]);
